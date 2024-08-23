faa054f18fa08b1a0c7d31aba30ae51e83902d67
ImNaveen <naveendev004@gmail.com>
2024-08-23 07:52:33 +0530

    auth screen completed

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name='sign-in'
+          options={{
+            headerShown:false,
+            animation:'slide_from_left'
+
+          }}
+        />
+          <Stack.Screen
+          name='sign-up'
+          options={{
+            headerShown:false,
+            animation:'slide_from_right'
+          }}
+          
+        />
+      </Stack>
+
+      <StatusBar backgroundColor='#161622' style='light' />
+    </>
   )
 }
 
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton  from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignUp = () => {
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form,setForm] = useState({
+    username:'',
+    email:"",
+    password:"",
+  });
+
+
+  const submit =()=>{
+
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+
+        <View className='w-full flex justify-center min-h-[95vh] px-4 my-8'>
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Sign Up to Aora
+          </Text>
+          <FormField
+             title="Username"
+             value={form.username}
+             handleChangeText={(e)=> setForm({...form,username:e})}
+             otherStyles="mt-10"
+           
+          />
+
+          <FormField
+             title="Email"
+             value={form.email}
+             handleChangeText={(e)=> setForm({...form,email:e})}
+             otherStyles="mt-7"
+             keyboardType="email-address"
+          />
+
+          <FormField
+            title={'Password'}
+            value={form.password}
+            handleChangeText={()=> setForm({...form,password:e})}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton 
+             title="Sign In"
+             handlePress={submit}
+             containerStyles="mt-7"
+             isLoading={isSubmitting}
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link href={"/sign-in"}
+            className='text-lg font-psemibold text-secondary'
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
